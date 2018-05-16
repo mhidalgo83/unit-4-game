91ddf36f856a8410f4fcb685df83dc63893e988a
Matt <makeshifthidalgo@hotmail.com>
2018-05-16 11:34:40 -0700

    crystal buttons working and adding integers to game score

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,80 +1,78 @@
-// Score generator...
-var minNumber = 19;
+$(document).ready(function gameStart() {
+// Number generator...
 
-var maxNumber = 120;
+var randomNumber = randomNumberFromRange(19, 120);
 
-var randomNumber = randomNumberFromRange();
+var crystalOne = randomNumberFromRange(1, 12);
 
-function randomNumberFromRange() {
+var crystalTwo = randomNumberFromRange(1, 12);
+
+var crystalThree = randomNumberFromRange(1, 12);
+
+var crystalFour = randomNumberFromRange(1, 12);
+
+var wins = 0;
+$("#wins").html("Wins: " + wins);
+
+var losses = 0;
+$("#losses").html("Losses: " + losses);
+
+// Crystal total... 
+var crystalTotal = 0;
+var gameScore = crystalTotal;
+
+function randomNumberFromRange(minNumber, maxNumber) {
     return Math.floor(Math.random() * (maxNumber - minNumber + 1) + minNumber);
 }
 
+// Starting the game...
+function gameStart () {
+    randomNumber;
+    $("#game-score").html = 0;
+};
 
-// Crystal number generator...
-var mnNumber = 1;
 
-var mxNumber = 12;
 
-var rndmNumber = rndmNumberFromRange();
-
-function rndmNumberFromRange() {
-    return Math.floor(Math.random() * (mxNumber - mnNumber + 1) + mnNumber);
-}
-
-$(document).ready(function gameStart() {
     // Crystal buttons...
     $("#crystal-1").click(function () {
-        rndmNumberFromRange();
-        alert(rndmNumber);
+        crystalOne = parseInt(crystalOne);
+        crystalTotal = parseInt(crystalTotal);
+        crystalTotal = crystalOne + crystalTotal;
+        $("#game-score").html(crystalTotal);
     });
     $("#crystal-2").click(function () {
-        rndmNumberFromRange();
-        alert(rndmNumber);
+        crystalTwo = parseInt(crystalTwo);
+        crystalTotal = parseInt(crystalTotal);
+        crystalTotal = crystalTwo + crystalTotal;
+        $("#game-score").html(crystalTotal);
     });
     $("#crystal-3").click(function () {
-        rndmNumberFromRange();
-        alert(rndmNumber);
+        crystalThree = parseInt(crystalThree);
+        crystalTotal = parseInt(crystalTotal);
+        crystalTotal = crystalThree + crystalTotal;
+        $("#game-score").html(crystalTotal);
     });
     $("#crystal-4").click(function () {
-        rndmNumberFromRange();
-        alert(rndmNumber);
-
+        crystalFour = parseInt(crystalFour);
+        crystalTotal = parseInt(crystalTotal);
+        crystalTotal = crystalFour + crystalTotal;
+        $("#game-score").html(crystalTotal);
     });
-    // Keeping score...
+    
 
-    var wins = 0;
-    $("#wins").html("Wins: " + wins);
-    var losses = 0;
-    $("#losses").html("Losses: " + losses);
-    // Crystal total... 
-    var crystalTotal = 0;
-
-    $("#random-number").onkeyup (function randomNumberFromRange() {
-        $("#random-number").html(randomNumberFromRange());
-    });
-
+    
 });
 
-
-
-
-
-
-
-
-
-
-// Crystal buttons...
 
 
 if (crystalTotal == randomNumber) {
     wins++;
     document.getElementById("wins").innerHTML = "Wins: " + wins;
-    startGame();
+    gameStart();
 }
 
 else if (crystalTotal > randomNumber) {
     losses++;
     document.getElementById("losses").innerHTML = "Losses: " + losses;
-    startGame();
-}
+    gameStart();
+};
